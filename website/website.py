4ab8b36f802a37f95ffcfd323178b28987c69cf2
Ramon de Klein <mail@ramondeklein.nl>
2015-01-11 01:18:02 +0100

    Using Flask for REST service

--- website/website.py
+++ website/website.py
@@ -12,47 +12,70 @@ import sys
 sys.path.append('..')
 
 # Import modules
-from helpers import Database, Settings
-from os import curdir
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import datetime, dateutil.parser, pytz
+from flask import Flask, request, jsonify
+from helpers import Database
 
-def sanitize(path):
-  # TODO: Implement URL sanitazation
-  if path == '/':
-    path = '/index.html'
-  return path
+# Declare Flask application
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
-class MyHandler(BaseHTTPRequestHandler):
-  def do_GET(self):
-    try:
-      # Obtain the request path
-      path = sanitize(self.path)
-      if not path.startswith('/api'):
-        f = open('{}/content/{}'.format(curdir, path))
-        self.send_response(200)
-        content_type = 'application/octet-stream'
-	if path.endswith('.html'):
-          content_type = 'text/html'
-        self.send_header('Content-type', content_type)
-        self.end_headers()
-        self.wfile.write(f.read())
-        f.close()
-      else:
-        self.send_error(404, 'API not implemented yet.')
-    except IOError:
-      self.send_error(404, 'Path {} not found'.format(self.path))
-    except Exception as exc:
-      logger.error("Exception: {}".format(exc))
-      self.send_error(500, 'Exception caught (check server logging)')
+def get_period():
+  # Determine the start and end parameters (optional)
+  argStart = request.args.get('start')
+  argEnd = request.args.get('end')
 
-port = 8888
-server = None
-try:
-  server = HTTPServer(('', port), MyHandler)
-  print 'Listening on port {}...'.format(port)
-  server.serve_forever()
-except KeyboardInterrupt:
-  print ' received'
-finally:
-  print 'Shutting down...'
-  server.socket.close()
+  # Convert the start and end to Python types
+  start = dateutil.parser.parse(argStart) if argStart else None
+  end = dateutil.parser.parse(argEnd) if argEnd else None
+
+  # Convert defaults for start/end (default is last day and otherwise we select an entire day)
+  if not start and not end:
+    end = datetime.datetime.now(pytz.utc)
+  if not start and end:
+    start = end - datetime.timedelta(days=1)
+  elif start and not end:
+    end = start + datetime.timedelta(days=1)
+
+  # Convert datetime to UTC
+  return {
+    "utcStart": start.astimezone(pytz.utc),
+    "utcEnd": end.astimezone(pytz.utc)
+  }
+
+@app.route("/api/v1/pulses/<int:meter_id>")
+def get_pulses(meter_id):
+  # Obtain the period
+  period = get_period()
+  utcStart = period['utcStart']
+  utcEnd = period['utcEnd']
+
+  # Obtain pulse readings
+  cur = Database.cursor()
+  cur.execute("SELECT timestamp, delta FROM pulse_readings WHERE timestamp >= %s AND timestamp < %s ORDER BY timestamp", (utcStart, utcEnd));
+  rows = cur.fetchall()
+  return jsonify({
+    'start': utcStart.isoformat(),
+    'end': utcEnd.isoformat(),
+    'pulses': map(lambda r: [r[0].isoformat() + 'Z', r[1]], rows)
+  })
+
+@app.route("/api/v1/pulses/<int:meter_id>/<int:duration>")
+def get_duration(meter_id, duration):
+  # Obtain the period
+  period = get_period()
+  utcStart = period['utcStart']
+  utcEnd = period['utcEnd']
+
+  # Obtain duration readings
+  cur = Database.cursor()
+  cur.execute("SELECT timestamp, pulses FROM pulse_readings_per_duration WHERE timestamp >= %s AND timestamp < %s ORDER BY timestamp", (utcStart, utcEnd));
+  rows = cur.fetchall()
+  return jsonify({
+    'start': utcStart.isoformat(),
+    'end': utcEnd.isoformat(),
+    'pulses': map(lambda r: [r[0].isoformat() + 'Z', r[1]], rows)
+  })
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=80, debug=True)
